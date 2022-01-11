a0f67b65b61dc590e6d0001f30d8f3daaf635335
Aryan <sharmaryan089@gmail.com>
2022-01-11 16:40:37 +0530

    Commit
    Commiting

---
+++ main.py
@@ -0,0 +1,20 @@
+first = input("Enter first number: ")
+operators = input("Enter operator (+, -, *, /)")
+second = input("Enter second number: ")
+
+first = int(first)
+second = int(second)
+
+if operator == "+":
+  print(first + second)
+elif operator == "-":
+  print(first - second)
+elif operator == "*":
+  print(first * second)
+elif operator == "/":
+  print(first / second)
+else:
+  print("Invalid operation!")
+
+# Thanks for using
+print("Thanks for using our system!")
